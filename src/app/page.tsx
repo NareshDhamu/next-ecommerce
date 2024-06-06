db70206ee6fd77b1b76137c106b8d8fe342e697f
Naresh <nareshsu427@gmail.com>
2024-06-06 16:17:59 +0530

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ const HomePage = async () => {
       <div className="mt-24 px-4 md:px-8 lg:px-16 2xl:px-64 xl:px-32">
         <h1 className="text-2xl">Featured Products</h1>
         <Suspense fallback={<div>Loading...</div>}>
-          <ProductList catecgoryId={process.env.DATA} limit={4} />
+          <ProductList catecgoryId="b5bdfd12-e9b8-50a5-3bb9-69f2a0c2240b" limit={4} />
         </Suspense>
       </div>
       <div className="mt-24">

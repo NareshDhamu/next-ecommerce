358974b5efaeb64e86329e304431942d99221a8a
Naresh <nareshsu427@gmail.com>
2024-06-08 11:51:03 +0530

    update

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -55,7 +55,7 @@ const Slider = () => {
     return () => clearInterval(interval);
   }, []);
   return (
-    <div className="h-[calc(100vh-80px)] overflow-hidden">
+    <div className="h-screen overflow-hidden">
       <div
         className="w-max h-full flex transition-all ease-in-out duration-1000"
         style={{ transform: `translateX(-${current * 100}vw)` }}

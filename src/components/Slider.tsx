e5cb75acd9d37f6205c3d806910c27794402f21d
Naresh <nareshsu427@gmail.com>
2024-06-08 11:33:25 +0530

    update

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -5,42 +5,45 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const slides = [
-    {
-      id: 1,
-      title: "Explore the Mountains",
-      description: "Discover the serene beauty of the majestic mountains and find your inner peace.",
-      image:
-        "https://images.pexels.com/photos/4049876/pexels-photo-4049876.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      url: "/",
-      bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
-    },
-    {
-      id: 2,
-      title: "Sunset Bliss",
-      description: "Experience the tranquil beauty of a sunset over the rolling hills.",
-      image:
-        "https://images.pexels.com/photos/4050388/pexels-photo-4050388.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      url: "/",
-      bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
-    },
-    {
-      id: 3,
-      title: "Forest Pathway",
-      description: "Take a walk through the lush forest and breathe in the fresh air of nature.",
-      image: "https://images.pexels.com/photos/3756345/pexels-photo-3756345.jpeg",
-      url: "/",
-      bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
-    },
-    {
-      id: 4,
-      title: "Serene Lake",
-      description: "Find peace and calm by the tranquil waters of a serene lake.",
-      image: "https://images.pexels.com/photos/3794178/pexels-photo-3794178.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      url: "/",
-      bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
-    },
-  ];
-  
+  {
+    id: 1,
+    title: "Explore the Mountains",
+    description:
+      "Discover the serene beauty of the majestic mountains and find your inner peace.",
+    image:
+      "https://images.pexels.com/photos/4049876/pexels-photo-4049876.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    url: "/",
+    bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
+  },
+  {
+    id: 2,
+    title: "Sunset Bliss",
+    description:
+      "Experience the tranquil beauty of a sunset over the rolling hills.",
+    image:
+      "https://images.pexels.com/photos/4050388/pexels-photo-4050388.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    url: "/",
+    bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
+  },
+  {
+    id: 3,
+    title: "Forest Pathway",
+    description:
+      "Take a walk through the lush forest and breathe in the fresh air of nature.",
+    image: "https://images.pexels.com/photos/3756345/pexels-photo-3756345.jpeg",
+    url: "/",
+    bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
+  },
+  {
+    id: 4,
+    title: "Serene Lake",
+    description: "Find peace and calm by the tranquil waters of a serene lake.",
+    image:
+      "https://images.pexels.com/photos/3794178/pexels-photo-3794178.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    url: "/",
+    bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
+  },
+];
 
 const Slider = () => {
   const [current, setCurrent] = useState(0);
@@ -53,8 +56,10 @@ const Slider = () => {
   }, []);
   return (
     <div className="h-[calc(100vh-80px)] overflow-hidden">
-      <div className="w-max h-full flex transition-all ease-in-out duration-1000"
-      style={{ transform: `translateX(-${current * 100}vw)` }}>
+      <div
+        className="w-max h-full flex transition-all ease-in-out duration-1000"
+        style={{ transform: `translateX(-${current * 100}vw)` }}
+      >
         {slides.map((slide) => (
           <div
             className={`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`}
@@ -85,20 +90,22 @@ const Slider = () => {
           </div>
         ))}
       </div>
-      <div className="absolute m-auto left-1/2 bottom-5 flex gap-4">
-        {slides.map((slide, index) => (
-          <div
-            className={`w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
-              current === index ? "scale-150" : ""
-            }`}
-            key={slide.id}
-            onClick={() => setCurrent(index)}
-          >
-            {current === index && (
-              <div className="w-[6px] h-[6px] rounded-full bg-gray-600"></div>
-            )}
-          </div>
-        ))}
+      <div className="w-full flex justify-center">
+        <div className=" absolute bottom-5 flex gap-4 bg-white px-3 py-2 rounded-full">
+          {slides.map((slide, index) => (
+            <div
+              className={`w-2.5 h-2.5 rounded-full ring-1 ring-green-600 cursor-pointer flex items-center justify-center ${
+                current === index ? "scale-150" : ""
+              }`}
+              key={slide.id}
+              onClick={() => setCurrent(index)}
+            >
+              {current === index && (
+                <div className="w-[7px] h-[7px] rounded-full bg-green-600"></div>
+              )}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
